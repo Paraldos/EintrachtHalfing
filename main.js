44f4a57b38bb2e5c3103caa69427ffac5c49d85c
Paraldos <hillbrecht.s@gmail.com>
2022-11-01 17:22:04 +0100

    images in articles and scrolling

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const MENU_BTN = document.querySelector(".menu_btn")
 const NAVBAR = document.querySelector(".navbar")
+const NAV_LINKS = document.querySelector(".nav_links")
 
 MENU_BTN.addEventListener("click", () => {
     if (NAVBAR.classList.contains("open")) {
@@ -10,3 +11,12 @@ MENU_BTN.addEventListener("click", () => {
         NAVBAR.classList.add("open")
     }
 })
+
+
+
+for(let link of NAV_LINKS.children) {
+    link.addEventListener("click", () => {
+        NAVBAR.classList.remove("open")
+    })
+    console.log(link)
+}
